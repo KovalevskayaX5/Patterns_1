7247f7bfe067c8337b481c8f347dcea851f60fb9
YanaK <Yana.Koval15@yandex.ru>
2023-05-13 21:26:27 +0300

    add tests

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -13,38 +13,33 @@ public class DataGenerator {
     private DataGenerator() {
     }
 
+
     public static String generateDate(int shift) {
-        // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
-        // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
-        return date;
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
-    public static String generateCity(String locale) {
-        // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
-        // с помощью Faker, либо используя массив валидных городов и класс Random
-        return city;
+    public static String generateCity() {
+        var cities = new String[]{"Санкт-Петербург", "Саранск", "Элиста", "Москва", "Ростов-на-Дону", "Ставрополь", "Калининград", "Красноярск"};
+        return cities[new Random().nextInt(cities.length)];
     }
 
     public static String generateName(String locale) {
-        // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
-        // использовать Faker
-        return name;
+        var faker = new Faker(new Locale(locale));
+        return faker.name().lastName() + " " + faker.name().firstName();
     }
 
     public static String generatePhone(String locale) {
-        // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
-        // использовать Faker
-        return phone;
+        var faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().phoneNumber();
     }
 
     public static class Registration {
+
         private Registration() {
         }
 
         public static UserInfo generateUser(String locale) {
-            // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
-            // generateName(locale), generatePhone(locale)
-            return user;
+            return new DataGenerator.UserInfo(generateCity(), generateName(locale), generatePhone(locale));
         }
     }
 
@@ -53,6 +48,5 @@ public class DataGenerator {
         String city;
         String name;
         String phone;
-
     }
 }
